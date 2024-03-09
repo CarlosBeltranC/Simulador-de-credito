7026c22a71bac2364de67fd9ea78ad3042d7b0ee
CarlosBeltranC <carlosbeltran.ac@academia.umb.edu.co>
2024-03-09 08:58:59 -0500

    Update

---
+++ app/src/main/java/com/example/simuladorcredito/ResultadoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.simuladorcredito
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.resultado)
+    }
+}
